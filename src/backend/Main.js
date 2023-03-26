5e0c8c609a3a51c759871fd23a27daba68384772
Mustaq Mujahid Mim <mustaqmujahidmim@gmail.com>
2023-03-27 02:47:03 +0600

    express

---
+++ src/backend/Main.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const mysql = require('mysql');
+const session = require('express-session');
+const MySQLStore = require('express-mysql-session')(session);
+const Router = require('./Router');
+
+app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.json());
+console.log('Testing server');
